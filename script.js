aabee036fb8fd0e4741bc3688a164f9b528db516
Kyle J Carlton <kylejcarlton@gmail.com>
2018-01-24 15:13:49 -0600

    basic layout and styling, define endGameEval

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 $(document).ready(function(){
-  var aiPlayer, huPlayer, board = [0,1,2,3,4,5,6,7,8];
+  var aiPlayer, huPlayer, board = [0,1,2,3,4,5,6,7,8], availSpots = 9;
+
+  function endGameEval(player, board){
+    if(
+      (player == board[0] == board[1] == board [2]) ||
+      (player == board[3] == board[4] == board [5]) ||
+      (player == board[6] == board[7] == board [8]) ||
+      (player == board[0] == board[3] == board [6]) ||
+      (player == board[1] == board[4] == board [7]) ||
+      (player == board[2] == board[5] == board [8]) ||
+      (player == board[0] == board[4] == board [8]) ||
+      (player == board[6] == board[4] == board [2])
+    ){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+
+  }
 
 }); //END DOC READY
