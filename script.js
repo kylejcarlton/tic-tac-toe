9a067ba8bc47f1ef49e1844fc89c48bfa9325f3a
Kyle J Carlton <kylejcarlton@gmail.com>
2018-02-13 16:43:50 -0600

    More work on logic for miniMax

--- script.js
+++ script.js
@@ -2,13 +2,49 @@ $(document).ready(function(){
   var aiPlayer, huPlayer, board = [0,1,2,3,4,5,6,7,8], availMoves = [], potentialMoves = [], moves = [];
   aiPlayer = "x";
   huPlayer = "o"; //Need to define functions that ties these into initial menu
+
   function aiMove(board, aiPlayer){
     potentialMoves = availSpots(board);
     for(j=0; j<potentialMoves.length; j++){
       var potentialBoard = board.slice(0);
       potentialBoard[potentialMoves[j]] = aiPlayer;
-      moves.push({option:j,move:aiPlayer,board:potentialBoard});
+      moves.push({option:j,move:aiPlayer,board:potentialBoard, rating:0});
       miniMax(moves[j]);
+    }
+  }
+  function availSpots(board){
+    availMoves = [];
+    for(i=0; i<=board.length; i++){
+      if(typeof(board[i]) == 'number'){
+        availMoves.push(board[i]);
+      }
+    }
+    return availMoves;
+  }
+  function miniMax(pMove){
+    if(endGameEval(pMove.move, pMove.board) == false){
+      //change player
+      switch(pMove.move){
+        case "x":
+          pMove.move = "o";
+          break;
+        case "o":
+          pMove.move = "x";
+          break;
+      }
+      //calculate available spots
+      //console.log(availSpots(pMove.board));
+      //assign changed player to each spot in each array value, then pass each new board back to miniMax for evaluation
+      for(k=0; k<availSpots(pMove.board).length; k++){
+        console.log(pMove.board);
+        console.log(k);
+        pMove.board[availSpots(pMove.board)[k]] = pMove.move;
+        console.log(pMove.board);
+
+      }
+    }
+    else{
+      console.log("End State of option: "+pMove.option);
     }
   }
   function endGameEval(player, board){
@@ -26,28 +62,6 @@ $(document).ready(function(){
       return true;
     } else {
       return false;
-    }
-  }
-  function availSpots(board){
-    availMoves = [];
-    for(i=0; i<=board.length; i++){
-      if(typeof(board[i]) == 'number'){
-        availMoves.push(board[i]);
-      }
-    }
-    return availMoves;
-  }
-  function miniMax(pMove){
-    if(endGameEval(pMove.move, pMove.board) == false){
-      console.log("true");
-      //change player
-      //if()
-      //calculate available spots
-      //assign changed player to each spot in each array value
-      //pass each new board back to miniMax for evaluation
-    }
-    else{
-      console.log("The End");
     }
   }
 
